2e589db82edd2b163c27e4a39782a86200ca99fb
jabin-v <jabinv03@gmail.com>
2024-04-16 15:34:02 +0530

    add logo and select box

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <h1>Welcome to pizza shop</h1>
-    </main>
+    </>
   );
 }
