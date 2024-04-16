d1968a9cbbb1ef6d09c4656617eaaf75d666747a
jabin-v <jabinv03@gmail.com>
2024-04-16 16:18:12 +0530

    add category tabs

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 
 export default function Home() {
@@ -26,6 +27,22 @@ export default function Home() {
               height={400}
             />
           </div>
+        </div>
+      </section>
+      <section>
+        <div className="container py-12 ">
+          <Tabs defaultValue="pizza" className="w-[400px]">
+            <TabsList>
+              <TabsTrigger value="pizza" className="text-md">
+                Pizza
+              </TabsTrigger>
+              <TabsTrigger className="text-md" value="beverages">
+                Beverages
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="pizza">pizza list</TabsContent>
+            <TabsContent value="beverages">beaverages list</TabsContent>
+          </Tabs>
         </div>
       </section>
     </>
