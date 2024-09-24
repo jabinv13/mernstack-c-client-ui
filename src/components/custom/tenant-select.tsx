77e46cb72f185a191da1a5b4d2912c5b7bac9ab7
jabin-v <jabinv03@gmail.com>
2024-09-24 10:53:13 +0530

    Set select on refresh

--- src/components/custom/tenant-select.tsx
+++ src/components/custom/tenant-select.tsx
@@ -8,21 +8,27 @@ import {
   SelectValue,
 } from "../ui/select";
 import { Tenant } from "@/lib/types";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const TenantSelect = ({ restaurants }: { restaurants: { data: Tenant[] } }) => {
   const router = useRouter();
+  const searchParams = useSearchParams();
+
+  console.log(searchParams);
   const handleValueChange = (value: string) => {
     router.push(`/?restaurantId=${value}`);
   };
   return (
-    <Select onValueChange={handleValueChange}>
+    <Select
+      onValueChange={handleValueChange}
+      defaultValue={searchParams.get("restaurantId") || ""}
+    >
       <SelectTrigger className="w-[180px] focus:ring-0">
         <SelectValue placeholder="Select restaurant" />
       </SelectTrigger>
       <SelectContent>
         {restaurants.data.map((restaurant) => (
-          <SelectItem key={restaurant.id} value={restaurant.id}>
+          <SelectItem key={restaurant.id} value={String(restaurant.id)}>
             {restaurant.name}
           </SelectItem>
         ))}
