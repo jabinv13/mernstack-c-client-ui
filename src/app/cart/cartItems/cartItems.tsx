07443575d3d885585f46f2b5f0834598e210f0f2
jabin-v <jabinv03@gmail.com>
2024-09-24 13:18:34 +0530

    add final total

--- src/app/cart/cartItems/cartItems.tsx
+++ src/app/cart/cartItems/cartItems.tsx
@@ -6,6 +6,7 @@ import { useAppSelector } from "@/lib/store/hooks";
 import { ArrowRight, ShoppingCart } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { getItemTotal } from "@/lib/utils";
 
 const CartItems = () => {
   const searchParams = useSearchParams();
@@ -17,11 +18,11 @@ const CartItems = () => {
   }, []);
   const cart = useAppSelector((state) => state.cart.cartItems);
 
-  //   const finalTotal = React.useMemo(() => {
-  //     return cart.reduce((acc, curr) => {
-  //       return acc + curr.qty * getItemTotal(curr);
-  //     }, 0);
-  //   }, [cart]);
+  const finalTotal = React.useMemo(() => {
+    return cart.reduce((acc, curr) => {
+      return acc + curr.qty * getItemTotal(curr);
+    }, 0);
+  }, [cart]);
 
   if (!isClient) {
     return null;
@@ -50,7 +51,7 @@ const CartItems = () => {
         <CartItem key={cartItem.hash} item={cartItem} />
       ))}
       <div className="flex justify-between items-center">
-        <span className="font-bold text-xl">4500</span>
+        <span className="font-bold text-xl">&#8377;{finalTotal}</span>
         <Button
           onClick={() =>
             router.push(
