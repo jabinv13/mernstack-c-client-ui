491e330f593044551748731c6426a7c952999436
jabin-v <jabinv03@gmail.com>
2024-09-24 13:16:01 +0530

    optimse use totalt

--- src/lib/hooks/useTotal.tsx
+++ src/lib/hooks/useTotal.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { CartItem } from "../store/features/cart/cartSlice";
+import { getItemTotal } from "../utils";
 
 export function useTotal(product: CartItem) {
   const totalPrice = React.useMemo(() => {
-    const toppingsTotal = product.chosenConfiguration.selectedToppings.reduce(
-      (acc, curr) => acc + curr.price,
-      0
-    );
-    const configPricing = Object.entries(
-      product.chosenConfiguration.priceConfiguration
-    ).reduce((acc, [key, value]: [string, string]) => {
-      const price = product.priceConfiguration[key].availableOptions[value];
-      return acc + price;
-    }, 0);
-    return configPricing + toppingsTotal;
+    // const toppingsTotal = product.chosenConfiguration.selectedToppings.reduce(
+    //   (acc, curr) => acc + curr.price,
+    //   0
+    // );
+    // const configPricing = Object.entries(
+    //   product.chosenConfiguration.priceConfiguration
+    // ).reduce((acc, [key, value]: [string, string]) => {
+    //   const price = product.priceConfiguration[key].availableOptions[value];
+    //   return acc + price;
+    // }, 0);
+    // return configPricing + toppingsTotal;
+    return getItemTotal(product);
   }, [product]);
   return totalPrice;
 }
