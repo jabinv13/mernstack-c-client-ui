45906292bc9658109133c0d769d0ebf49c02b744
jabin-v <jabinv03@gmail.com>
2024-10-21 14:29:09 +0530

    dynamical rendering order status

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -21,51 +21,73 @@ const Payment = ({
   return (
     <div className="flex flex-col items-center gap-4 w-full mt-20">
       {isOrderSuccess ? (
-        <CheckCircle2 size={80} className="text-green-500" />
+        <>
+          <CheckCircle2 size={80} className="text-green-500" />
+          <h1 className="text-2xl front-bold mt-2 text-center ">
+            Order placed successfully
+          </h1>
+          <p className="text-base font-semibold">Thank you for your order .</p>
+        </>
       ) : (
-        <CircleX size={80} className="text-green-500" />
+        <>
+          <CircleX size={80} className="text-red-500" />
+          <h1 className="text-2xl front-bold mt-2 text-center ">
+            Payment has been falied
+          </h1>
+          <p className="text-base font-semibold">Thank you for your order .</p>
+        </>
       )}
 
-      <h1 className="text-2xl front-bold mt-2 text-center ">
-        Order placed successfully
-      </h1>
-      <p className="text-base font-semibold">Thank you for your order .</p>
-
-      <Card className="mt-6">
-        <CardHeader className="p-4 ">
-          <CardTitle className="flex items-start text-lg  justify-between">
-            <div className="flex items-center gap-3">
-              <Store size={35} className="text-primary" />
-              <span>Your order information</span>
+      {isOrderSuccess && (
+        <Card className="mt-6">
+          <CardHeader className="p-4">
+            <CardTitle className="flex items-start text-lg justify-between gap-12">
+              <div className="flex item-center gap-3">
+                <Store size={35} className="text-primary" />
+                <span>Your order information</span>
+              </div>
+              <Badge className="text-base px-4" variant={"secondary"}>
+                Confirmed
+              </Badge>
+            </CardTitle>
+          </CardHeader>
+          <Separator />
+          <CardContent className="pt-6">
+            <div className="flex items-center gap-2">
+              <LayoutDashboard size={20} />
+              <h2 className="text-base font-medium">Order reference: </h2>
+              <Link
+                href={`/order/${searchParams.orderId}`}
+                className="underline"
+              >
+                {searchParams.orderId}
+              </Link>
             </div>
-            <Badge className="text-base px-4" variant={"secondary"}>
-              Confirmed
-            </Badge>
-          </CardTitle>
-        </CardHeader>
-        <Separator />
-        <CardContent className="pt-2">
-          <div className="flex items-center gap-2">
-            <LayoutDashboard size={20} />
-            <h2 className="text-base font-medium">Order reference :</h2>
-            <Link href="/" className="underline">
-              25656565
-            </Link>
-          </div>
-          <div className="flex items-center gap-2 mt-3">
-            <LayoutDashboard size={20} />
-            <h2 className="text-base font-medium">Payment status : :</h2>
-            <span>Paid</span>
-          </div>
-        </CardContent>
-      </Card>
 
-      <Button asChild className="mt-6 ">
-        <Link href="/" className="flex items-center gap-2">
-          <ArrowLeft size={20} className="text-white" />
-          <span>Place another order</span>
-        </Link>
-      </Button>
+            <div className="flex items-center gap-2 mt-2">
+              <LayoutDashboard size={20} />
+              <h2 className="text-base font-medium">Payment status: </h2>
+              <span>Paid</span>
+            </div>
+          </CardContent>
+        </Card>
+      )}
+
+      {isOrderSuccess ? (
+        <Button asChild className="mt-6">
+          <Link href="/" className="flex items-center gap-2">
+            <ArrowLeft size={20} className="text-white" />
+            <span>Go to order status page</span>
+          </Link>
+        </Button>
+      ) : (
+        <Button asChild className="mt-6">
+          <Link href="/" className="flex items-center gap-2">
+            <ArrowLeft size={20} className="text-white" />
+            <span>Go to checkout</span>
+          </Link>
+        </Button>
+      )}
     </div>
   );
 };
